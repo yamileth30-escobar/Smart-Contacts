8698fa90545408bdd58ccbb4e7c98c80420d1491
yamileth30-escobar <ve1105032024@unab.edu.sv>
2026-04-10 21:02:56 -0600

    Definición de la Capa Entidades de Objetos

---
+++ EL/Contacto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaContactos.EL
+{
+    public class Contacto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Direccion { get; set; }
+    }
+}
