511c42576eae0737b0df0b0137b49fa4c52f6591
yamileth30-escobar <ve1105032024@unab.edu.sv>
2026-04-10 21:01:17 -0600

    Implementación de la Capa Lógica de Negocio

---
+++ BLL/ContactoBLL.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using AgendaContactos.DAL;
+using AgendaContactos.EL;
+
+namespace AgendaContactos.BLL
+{
+    public class ContactoBLL
+    {
+        private readonly ContactoDAL dal = new ContactoDAL();
+
+        public void Guardar(Contacto c) => dal.Insertar(c);
+        public DataTable Listar() => dal.Listar();
+        public void Editar(Contacto c) => dal.Actualizar(c);
+        public void Eliminar(int id) => dal.Eliminar(id);
+        public DataTable Buscar(string valor) => dal.Buscar(valor);
+    }
+}
---
+++ BLL/UsuarioBLL.cs
@@ -0,0 +1,15 @@
+﻿using AgendaContactos.DAL;
+using AgendaContactos.EL;
+
+namespace AgendaContactos.BLL
+{
+    public class UsuarioBLL
+    {
+        UsuarioDAL dal = new UsuarioDAL();
+
+        public bool Login(Usuario u)
+        {
+            return dal.Login(u);
+        }
+    }
+}
