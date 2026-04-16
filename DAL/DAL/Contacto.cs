b0a3b0505299cd9984264a82ce4a7580319aaf44
yamileth30-escobar <ve1105032024@unab.edu.sv>
2026-04-16 08:01:20 -0600

    Implementar métodos de persistencia y acceso a datos (DAL)

---
+++ DAL/DAL/Contacto.cs
@@ -0,0 +1,11 @@
+﻿namespace AgendaContactos.DAL
+{
+    public class Contacto
+    {
+        public string Categoria { get; internal set; }
+        public string Correo { get; internal set; }
+        public string Telefono { get; internal set; }
+        public string Nombre { get; internal set; }
+        public object Id { get; internal set; }
+    }
+}
