a01e5bcbeb0b88a1adbe28c962733c94a2f3e867
yamileth30-escobar <ve1105032024@unab.edu.sv>
2026-04-16 08:02:28 -0600

    Definir entidades y modelos de datos (EL)

---
+++ EL/EL/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaContactos.EL
+{
+    public class Categoria
+    {
+        public int IdCategoria { get; set; }
+        public string NombreCategoria { get; set; }
+    }
+}
