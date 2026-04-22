d9a86c2613eeb335b0826e9715d93f3f630e936c
JAGC60 <gc0715032022@unab.edu.sv>
2026-04-21 21:37:13 -0600

    apartado de lista de contactos arreglado

--- GUI/Contactos/FrmListaContactos.Designer.cs
+++ GUI/Contactos/FrmListaContactos.Designer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using AgendaContactos.GUI.Contactos;
+using System;
 using System.Windows.Forms;
 
 namespace GUI.Contactos
@@ -45,9 +46,9 @@ namespace GUI.Contactos
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(32, 20);
+            this.label1.Location = new System.Drawing.Point(36, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(267, 32);
+            this.label1.Size = new System.Drawing.Size(306, 38);
             this.label1.TabIndex = 0;
             this.label1.Text = "Lista de Contactos";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -59,27 +60,33 @@ namespace GUI.Contactos
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvContactos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvContactos.Location = new System.Drawing.Point(38, 78);
+            this.dgvContactos.Location = new System.Drawing.Point(43, 98);
+            this.dgvContactos.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dgvContactos.Name = "dgvContactos";
             this.dgvContactos.RowHeadersWidth = 51;
             this.dgvContactos.RowTemplate.Height = 24;
-            this.dgvContactos.Size = new System.Drawing.Size(676, 299);
+            this.dgvContactos.Size = new System.Drawing.Size(760, 374);
             this.dgvContactos.TabIndex = 1;
+            this.dgvContactos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvContactos_CellContentClick);
+            this.dgvContactos.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dvgContactos_CellDoubleClick);
             // 
             // btnActualizar
             // 
-            this.btnActualizar.Location = new System.Drawing.Point(52, 402);
+            this.btnActualizar.Location = new System.Drawing.Point(58, 502);
+            this.btnActualizar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnActualizar.Name = "btnActualizar";
-            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.Size = new System.Drawing.Size(84, 29);
             this.btnActualizar.TabIndex = 2;
             this.btnActualizar.Text = "Actualizar";
             this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
             // 
             // btnNuevo
             // 
-            this.btnNuevo.Location = new System.Drawing.Point(177, 402);
+            this.btnNuevo.Location = new System.Drawing.Point(199, 502);
+            this.btnNuevo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnNuevo.Name = "btnNuevo";
-            this.btnNuevo.Size = new System.Drawing.Size(122, 23);
+            this.btnNuevo.Size = new System.Drawing.Size(137, 29);
             this.btnNuevo.TabIndex = 3;
             this.btnNuevo.Text = "Nuevo Contacto";
             this.btnNuevo.UseVisualStyleBackColor = true;
@@ -87,36 +94,42 @@ namespace GUI.Contactos
             // 
             // btnEditar
             // 
-            this.btnEditar.Location = new System.Drawing.Point(344, 402);
+            this.btnEditar.Location = new System.Drawing.Point(387, 502);
+            this.btnEditar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnEditar.Name = "btnEditar";
-            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.Size = new System.Drawing.Size(84, 29);
             this.btnEditar.TabIndex = 4;
             this.btnEditar.Text = "Editar";
             this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
             // 
             // btnEliminar
             // 
-            this.btnEliminar.Location = new System.Drawing.Point(493, 402);
+            this.btnEliminar.Location = new System.Drawing.Point(555, 502);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.Size = new System.Drawing.Size(84, 29);
             this.btnEliminar.TabIndex = 5;
             this.btnEliminar.Text = "Eliminar";
             this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
             // btnCerrar
             // 
-            this.btnCerrar.Location = new System.Drawing.Point(629, 402);
+            this.btnCerrar.Location = new System.Drawing.Point(708, 502);
+            this.btnCerrar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnCerrar.Name = "btnCerrar";
-            this.btnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCerrar.Size = new System.Drawing.Size(84, 29);
             this.btnCerrar.TabIndex = 6;
             this.btnCerrar.Text = "Cerrar";
             this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click_1);
             // 
             // FrmListaContactos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(900, 562);
             this.Controls.Add(this.btnCerrar);
             this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.btnEditar);
@@ -124,6 +137,7 @@ namespace GUI.Contactos
             this.Controls.Add(this.btnActualizar);
             this.Controls.Add(this.dgvContactos);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "FrmListaContactos";
             this.Text = "FrmListaContactos";
             ((System.ComponentModel.ISupportInitialize)(this.dgvContactos)).EndInit();
@@ -134,7 +148,10 @@ namespace GUI.Contactos
 
         private void button2_Click(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+          FrmAgregarContacto frm = new FrmAgregarContacto();
+          frm.ShowDialog();
+          CargarContactos();
+          throw new NotImplementedException();
         }
 
         private void Lista_Click(object sender, EventArgs e)
--- GUI/Contactos/FrmListaContactos.cs
+++ GUI/Contactos/FrmListaContactos.cs
@@ -1,124 +1,196 @@
-﻿using System;
+using AgendaContactos.GUI.Contactos;
+using DAL; // Usando tu capa de datos
+using System;
+using System.Data;
 using System.Data.SqlClient;
 using System.Windows.Forms;
-using DAL;                    // Asegúrate que este namespace coincida con tu capa DAL
 
 namespace GUI.Contactos
 {
-    public partial class FrmListaContactos : Form
+  public partial class FrmListaContactos : Form
+  {
+    public FrmListaContactos()
     {
-        public FrmListaContactos()
-        {
-            InitializeComponent();
-            this.Text = "Lista de Contactos";
-            this.WindowState = FormWindowState.Maximized;
-        }
-
-        private void FrmListaContactos_Load(object sender, EventArgs e)
-        {
-            // Probar conexión a la base de datos al cargar el formulario
-            ProbarConexionBaseDatos();
-
-            // Configuración inicial del DataGridView
-            ConfigurarDataGridView();
-
-            // TODO: Aquí más adelante cargarás los contactos reales desde la BLL
-            // CargarContactos();
-        }
-
-        /// <summary>
-        /// Prueba la conexión a la base de datos
-        /// </summary>
-        private void ProbarConexionBaseDatos()
-        {
-            try
-            {
-                using (SqlConnection conn = new DAL.Conexion().ObtenerConexion())   // Versión más explícita
-                {
-                    conn.Open();
-                    MessageBox.Show("✅ ¡Conexión exitosa a la base de datos!\n\n" +
-                                   "Servidor: GUADALUPE\\GUADALUPE\n" +
-                                   "Base de datos: AgendaContactos",
-                                   "Conexión Exitosa",
-                                   MessageBoxButtons.OK,
-                                   MessageBoxIcon.Information);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("❌ Error de conexión:\n\n" + ex.Message,
-                               "Error",
-                               MessageBoxButtons.OK,
-                               MessageBoxIcon.Error);
-            }
-        }
-
-        private void ConfigurarDataGridView()
-        {
-            dgvContactos.Columns.Clear();
-            dgvContactos.Columns.Add("Id", "ID");
-            dgvContactos.Columns.Add("Nombre", "Nombre Completo");
-            dgvContactos.Columns.Add("Telefono", "Teléfono");
-            dgvContactos.Columns.Add("Email", "Correo Electrónico");
-            dgvContactos.Columns.Add("Categoria", "Categoría");
-
-            // Ajustar ancho de columnas
-            dgvContactos.Columns["Id"].Width = 60;
-            dgvContactos.Columns["Nombre"].Width = 200;
-            dgvContactos.Columns["Telefono"].Width = 120;
-            dgvContactos.Columns["Email"].Width = 180;
-            dgvContactos.Columns["Categoria"].Width = 120;
-
-            // Opcional: Mejorar apariencia
-            dgvContactos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            dgvContactos.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dgvContactos.MultiSelect = false;
-            dgvContactos.AllowUserToAddRows = false;
-            dgvContactos.ReadOnly = true;
-        }
-
-        private void btnActualizar_Click(object sender, EventArgs e)
-        {
-            // MessageBox.Show("Lista actualizada (simulación)", "Actualizar");
-            // TODO: Llamar al método que carga los contactos desde la BLL
-            // CargarContactos();
-        }
-
-        private void btnNuevo_Click(object sender, EventArgs e)
-        {
-            FrmAgregarContactos frm = new FrmAgregarContactos();
-            frm.ShowDialog();
-
-            // Después de agregar un contacto, actualizamos la lista automáticamente
-            btnActualizar_Click(null, null);
-        }
-
-        private void btnCerrar_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        // ==================== MÉTODO FUTURO ====================
-        // private void CargarContactos()
-        // {
-        //     // Aquí iría la llamada a la BLL
-        // }
+      InitializeComponent();
+      this.Text = "Lista de Contactos";
+      // Esto hará que se vea genial en tu MSI Katana
+      this.WindowState = FormWindowState.Maximized;
     }
 
-    // ==================== CLASE TEMPORAL (Eliminar cuando tengas el diseñador) ====================
-    public class FrmAgregarContactos : Form
+    private void FrmListaContactos_Load(object sender, EventArgs e)
     {
-        public FrmAgregarContactos()
-        {
-            InitializeComponent();
-        }
-
-        private void InitializeComponent()
-        {
-            this.Text = "Agregar Contacto";
-            this.StartPosition = FormStartPosition.CenterParent;
-            this.Width = 600;
-            this.Height = 400;
-        }
+      // 1. Cargamos los datos en cuanto se abre la ventana
+      CargarContactos();
     }
+
+    /// <summary>
+    /// Método principal para traer la info de SQL a la tabla
+    /// </summary>
+    private void CargarContactos()
+    {
+      try
+      {
+        // Usamos tu clase de conexión de la capa DAL
+        using (SqlConnection conn = new DAL.Conexion().ObtenerConexion())
+        {
+          conn.Open();
+
+          // Consulta SQL
+          string query = "SELECT IdContacto AS ID, Nombre, Apellido, Telefono, Correo FROM Contactos";
+
+          SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+          DataTable dt = new DataTable();
+          adapter.Fill(dt);
+
+          // Importante: Usamos el nombre que me confirmaste 'dvgContactos'
+          dgvContactos.DataSource = dt;
+
+          // Mejoramos el aspecto de la tabla
+          ConfigurarAparienciaTabla();
+        }
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show("❌ Error al cargar contactos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+      }
+    }
+
+    private void ConfigurarAparienciaTabla()
+    {
+      // Ajustes para que la tabla se vea pro
+      dgvContactos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+      dgvContactos.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+      dgvContactos.AllowUserToAddRows = false;
+      dgvContactos.ReadOnly = true;
+
+      // Opcional: Cambiar encabezados si querés que se vean más bonitos
+      if (dgvContactos.Columns.Count > 0)
+      {
+        dgvContactos.Columns["ID"].Width = 50;
+        dgvContactos.Columns["Correo"].HeaderText = "Correo Electrónico";
+      }
+    }
+
+    // --- BOTONES DEL DISEÑO ---
+
+    private void btnActualizar_Click(object sender, EventArgs e)
+    {
+      CargarContactos(); // Refresca la lista
+      MessageBox.Show("🔄 Lista actualizada correctamente.", "Sincronizado");
+    }
+
+    private void btnNuevo_Click(object sender, EventArgs e)
+    {
+      // Abrimos tu ventana de Agregar (FrmAgregarContacto)
+      // Nota: Asegurate que el nombre coincida con tu archivo (normalmente es sin la 's' al final)
+      FrmAgregarContacto frm = new FrmAgregarContacto();
+      frm.ShowDialog();
+
+      // Al volver de agregar, refrescamos automáticamente
+      CargarContactos();
+    }
+
+    private void btnCerrar_Click(object sender, EventArgs e)
+    {
+      this.Close();
+    }
+
+    // --- EXTRAS PARA EL DOBLE CLIC (Lo que hablamos antes) ---
+
+    private void dvgContactos_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+    {
+      if (e.RowIndex >= 0)
+      {
+        FrmEditarContacto frm = new FrmEditarContacto();
+
+        // Pasamos los datos a la ventana de editar
+        frm.txtIdEditar.Text = dgvContactos.Rows[e.RowIndex].Cells["ID"].Value.ToString();
+        frm.txtNombre.Text = dgvContactos.Rows[e.RowIndex].Cells["Nombre"].Value.ToString();
+        frm.txtApellido.Text = dgvContactos.Rows[e.RowIndex].Cells["Apellido"].Value.ToString();
+        frm.texTelefono.Text = dgvContactos.Rows[e.RowIndex].Cells["Telefono"].Value.ToString();
+
+        frm.ShowDialog();
+        CargarContactos(); // Refrescamos al terminar de editar
+      }
+    }
+
+    private void dgvContactos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+    {
+      CargarContactos();
+    }
+
+    private void btnActualizar_Click1(object sender, EventArgs e)
+    {
+      // Borrá el 'throw new...' y poné esto:
+      CargarContactos();
+      MessageBox.Show("🔄 Lista actualizada correctamente.", "Sincronizado");
+    }
+
+    private void btnCerrar_Click_1(object sender, EventArgs e)
+    {
+      this.Close();
+    }
+
+     private void btnEditar_Click(object sender, EventArgs e)
+    {
+      // Verificamos si hay una fila seleccionada
+      if (dgvContactos.SelectedRows.Count > 0)
+      {
+        FrmEditarContacto frm = new FrmEditarContacto();
+
+        // Pasamos los datos de la fila marcada a los cuadros de la ventana editar
+        frm.txtIdEditar.Text = dgvContactos.CurrentRow.Cells["ID"].Value.ToString();
+        frm.txtNombre.Text = dgvContactos.CurrentRow.Cells["Nombre"].Value.ToString();
+        frm.txtApellido.Text = dgvContactos.CurrentRow.Cells["Apellido"].Value.ToString();
+        frm.texTelefono.Text = dgvContactos.CurrentRow.Cells["Telefono"].Value.ToString();
+
+        frm.ShowDialog();
+        CargarContactos(); // Refresca la lista al cerrar
+      }
+      else
+      {
+        MessageBox.Show("Por favor, selecciona un contacto de la lista primero.");
+      }
+    }
+
+    private void btnEliminar_Click(object sender, EventArgs e)
+    {
+      if (dgvContactos.SelectedRows.Count > 0)
+      {
+        string id = dgvContactos.CurrentRow.Cells["ID"].Value.ToString();
+        string nombre = dgvContactos.CurrentRow.Cells["Nombre"].Value.ToString();
+
+        DialogResult res = MessageBox.Show("¿Seguro que querés borrar a " + nombre + "?",
+            "Confirmar", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+        if (res == DialogResult.Yes)
+        {
+          try
+          {
+            using (SqlConnection conn = new DAL.Conexion().ObtenerConexion())
+            {
+              conn.Open();
+              string sql = "DELETE FROM Contactos WHERE IdContacto = @id";
+              SqlCommand cmd = new SqlCommand(sql, conn);
+              cmd.Parameters.AddWithValue("@id", id);
+              cmd.ExecuteNonQuery();
+
+              MessageBox.Show("Contacto borrado.");
+              CargarContactos(); // Refresca la tabla
+            }
+          }
+          catch (Exception ex)
+          {
+            MessageBox.Show("Error: " + ex.Message);
+          }
+        }
+      }
+      else
+      {
+        MessageBox.Show("Selecciona a quién querés eliminar.");
+      }
+    }
+  }
+  
+  
 }
