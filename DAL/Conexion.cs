ffdd08091ac6aad2fe2c72654e43523d06d194ba
JAGC60 <gc0715032022@unab.edu.sv>
2026-04-22 19:45:21 -0600

    termine de arreglar el apastado de listacontactos

--- DAL/Conexion.cs
+++ DAL/Conexion.cs
@@ -1,15 +1,23 @@
-using System.Configuration;
+using System;
 using System.Data.SqlClient;
 
-namespace AgendaContactos.DAL
+namespace DAL
 {
+  internal class Conexion
+  {
+    // Esta es la llave que abre tu base de datos
+    // Usamos .\SQL como me dijiste
+    private string cadena = @"Data Source=.\SQL;Initial Catalog=AgendaContactos;Integrated Security=True";
 
-    public class Conexion
+    public Conexion()
     {
-        public static SqlConnection ObtenerConexion()
-        {
-            string cadenaConexion = ConfigurationManager.ConnectionStrings["AgendaContactos"].ConnectionString;
-            return new SqlConnection(cadenaConexion);
-        }
     }
+
+    // Este es el método que tus compañeros dejaron vacío
+    internal SqlConnection ObtenerConexion()
+    {
+      // Aquí devolvemos la conexión real hacia tu servidor
+      return new SqlConnection(cadena);
+    }
+  }
 }
--- GUI/DAL/Conexion.cs
+++ GUI/DAL/Conexion.cs
@@ -1,17 +1,22 @@
-﻿using System;
+using System;
 using System.Data.SqlClient;
 
 namespace DAL
 {
-    internal class Conexion
-    {
-        public Conexion()
-        {
-        }
+  internal class Conexion
+  {
+    // 1. Aquí guardamos la ruta a tu SQL Server
+    
+    string cadenaConexion = @"Data Source=.\SQL;Initial Catalog=AgendaContactos;Integrated Security=True";
 
-        internal SqlConnection ObtenerConexion()
-        {
-            throw new NotImplementedException();
-        }
+    public Conexion()
+    {
     }
+
+    internal SqlConnection ObtenerConexion()
+    {
+      // 2. Quitamos el error y devolvemos la conexión real
+      return new SqlConnection(cadenaConexion);
+    }
+  }
 }
