97b902fc540b7e91839465e95f0b5d915abbdb7a
luisescobar123 <el0598032025@unab.edu.sv>
2026-05-08 12:41:41 -0600

    Program.cs

--- AgendaContactos/Program.cs
+++ AgendaContactos/Program.cs
@@ -1,15 +1,21 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Namespace principal del proyecto AgendaContactos
 namespace AgendaContactos
 {
+    // Clase principal del programa
     internal class Program
     {
+        // Método principal donde inicia la ejecución de la aplicación
         static void Main(string[] args)
         {
+            // Aquí se escribe el código principal del programa
+
+            // args permite recibir argumentos desde la línea de comandos
         }
     }
 }
