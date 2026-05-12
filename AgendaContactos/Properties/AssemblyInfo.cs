08a188567927aa594232215f4f7b4c7fdfc26f76
luisescobar123 <el0598032025@unab.edu.sv>
2026-05-11 23:16:49 -0600

    AssemblyInfo.cs
    1. Configura la información del proyecto
    
    2. Define nombre y versión del sistema
    
    3. Agrega datos del autor o empresa
    
    4. Controla compatibilidad COM
    
    5. Identifica el ensamblado con un GUID
    
    6. Ayuda a Windows y Visual Studio a reconocer la aplicación correctamente

--- AgendaContactos/Properties/AssemblyInfo.cs
+++ AgendaContactos/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada a un ensamblado.
