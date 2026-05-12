752daebc5e8f08c532cfde276906b031df4611da
luisescobar123 <el0598032025@unab.edu.sv>
2026-05-11 23:19:38 -0600

    AssemblyInfo.cs
    Configura la información del proyecto DAL
    Define nombre y versión
    Configura compatibilidad COM
    Agrega identificador GUID
    Establece datos de autor y producto
    Controla la visibilidad del ensamblado

--- DAL/Properties/AssemblyInfo.cs
+++ DAL/Properties/AssemblyInfo.cs
@@ -2,9 +2,11 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada a un ensamblado.
+
 [assembly: AssemblyTitle("DAL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
