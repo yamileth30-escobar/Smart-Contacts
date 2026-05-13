6af21138514a89cfeb3ea8aea3868330f3e6d93b
luisescobar123 <el0598032025@unab.edu.sv>
2026-05-12 22:14:00 -0600

    Categoria.cs

--- EL/Categoria.cs
+++ EL/Categoria.cs
@@ -1,18 +1,30 @@
-﻿using System;
+﻿// Importación de librerías básicas del sistema
+using System;
+
+// Importación de colecciones genéricas
 using System.Collections.Generic;
+
+// Permite realizar consultas LINQ
 using System.Linq;
+
+// Proporciona clases para manejo de texto y codificación
 using System.Text;
+
+// Permite trabajar con tareas asíncronas
 using System.Threading.Tasks;
 
+// Espacio de nombres del proyecto AgendaContactos
 namespace AgendaContactos.EL
 {
-    // Solo debe decir "public class" una vez
+    // Clase que representa una categoría de contactos
     public class Categoria
     {
-        // Identificador único de la categoría (Ej: 1, 2, 3)
+        // Propiedad que almacena el identificador único de la categoría
+        // Ejemplo: 1, 2, 3
         public int Id { get; set; }
 
-        // Nombre de la categoría (Ej: Familia, Trabajo, Amigos)
+        // Propiedad que almacena el nombre de la categoría
+        // Ejemplo: Familia, Trabajo, Amigos
         public string Nombre { get; set; }
     }
 }
